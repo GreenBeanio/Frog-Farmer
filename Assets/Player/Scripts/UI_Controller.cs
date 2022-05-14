a138c059a5b1ef51e137b1828bde881687a79d7b
GreenBeanio <pingujohnson@gmail.com>
2022-05-13 23:38:56 -0400

    Starting stuff. Not much.
    Really not sure on what I'm doing much. It's been a hot minute since I tried to make a game.

---
+++ Assets/Player/Scripts/UI_Controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Controller : MonoBehaviour
+{
+    public TMP_Text healthText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Getting health from the player
+        int health = gameObject.GetComponent<Player_Controller>().health;
+        healthText.text = "Health: " + health;
+    }
+}
